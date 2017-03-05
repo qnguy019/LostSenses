2c829dfbae4788d0755d3100b68e1c30309bf446
dzhan008 <dzhan008@ucr.edu>
2017-03-04 18:25:45 -0800

    Updated Camera

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Player : MonoBehaviour {
 
     public float moveSpeed = 1f;
+    bool moving = true;
     public GameObject path;
 	// Use this for initialization
 	void Start () {
@@ -21,80 +22,85 @@ public class Player : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
-        if(Input.GetButton("up"))
+        if(moving)
         {
-            this.GetComponent<Rigidbody2D>().MovePosition(transform.position + Vector3.up * moveSpeed);
-        }	
-        else if(Input.GetButton("down"))
-        {
-            this.GetComponent<Rigidbody2D>().MovePosition(transform.position + Vector3.down * moveSpeed);
-        }	
-        else if(Input.GetButton("left"))
-        {
-            this.GetComponent<Rigidbody2D>().MovePosition(transform.position + Vector3.left * moveSpeed);
+            if (Input.GetButton("up"))
+            {
+                this.GetComponent<Rigidbody2D>().MovePosition(transform.position + Vector3.up * moveSpeed);
+            }
+            else if (Input.GetButton("down"))
+            {
+                this.GetComponent<Rigidbody2D>().MovePosition(transform.position + Vector3.down * moveSpeed);
+            }
+            else if (Input.GetButton("left"))
+            {
+                this.GetComponent<Rigidbody2D>().MovePosition(transform.position + Vector3.left * moveSpeed);
+            }
+            else if (Input.GetButton("right"))
+            {
+                this.GetComponent<Rigidbody2D>().MovePosition(transform.position + Vector3.right * moveSpeed);
+            }
+            if (Input.GetButtonDown("sight"))
+            {
+                if (path.activeInHierarchy) path.SetActive(false);
+                else path.SetActive(true);
+            }
         }
-        else if(Input.GetButton("right"))
-        {
-            this.GetComponent<Rigidbody2D>().MovePosition(transform.position + Vector3.right * moveSpeed);
-        }
-        if (Input.GetButtonDown("sight"))
-        {
-            if (path.activeInHierarchy) path.SetActive(false);
-            else path.SetActive(true);
-        }
+
         checkCamera();
 	}
 
     void checkCamera()
     {
-        Vector3 pos = Camera.main.WorldToScreenPoint(this.transform.position);
-        if(pos.x < 0.0)
+        Vector3 pos = Camera.main.WorldToViewportPoint(transform.position);
+        if (pos.x < 0.0)
         {
-            Debug.Log("Camera at left position!");
-            StartCoroutine("slideLeft");
+            StartCoroutine(slide(1.5f, "left", 10f));
         }
-        else if(1.0 < pos.x)
+        else if (1.0 < pos.x)
         {
-
+            StartCoroutine(slide(1.5f, "right", 10f));
         }
-        else if(pos.y < 0.0)
+        else if (pos.y < 0.0)
         {
-
+            StartCoroutine(slide(1.5f, "down", 10f));
         }
-        else if(1.0 < pos.y)
+        else if (1.0 < pos.y)
         {
-
+            StartCoroutine(slide(1.5f, "up", 10f));
         }
     }
 
-    IEnumerator slide(int time, string dir, int distance)
+    IEnumerator slide(float time, string dir, float distance)
     {
-        Debug.Log("Sliding");
         float elapsed_time = 0f;
         Vector3 startingPos = Camera.main.transform.position;
         Vector3 offset = new Vector3();
-        if(dir == "left")
+        moving = false;
+        if (dir == "left")
         {
-            offset = Camera.main.transform.position - new Vector3(distance, Camera.main.transform.position.y, 0f);
+            offset = Camera.main.transform.position - new Vector3(distance, 0f, 0f);
         }
         if (dir == "right")
         {
-            offset = Camera.main.transform.position - new Vector3(-distance, Camera.main.transform.position.y, 0f);
+            offset = Camera.main.transform.position + new Vector3(distance, 0f, 0f);
         }
         if (dir == "up")
         {
-            offset = Camera.main.transform.position - new Vector3(Camera.main.transform.position.x, distance, 0f);
+            offset = Camera.main.transform.position + new Vector3(0f, distance, 0f);
         }
         if (dir == "down")
         {
-            offset = Camera.main.transform.position - new Vector3(Camera.main.transform.position.x, -distance, 0f);
+            offset = Camera.main.transform.position - new Vector3(0f, distance, 0f);
         }
 
-        while(elapsed_time < time)
+        while (elapsed_time < time)
         {
-            Camera.main.transform.position = Vector3.Lerp(startingPos, offset, elapsed_time/time * 2.2f);
+            Camera.main.transform.position = Vector3.Lerp(startingPos, offset, elapsed_time / time * 2.2f);
             elapsed_time += Time.deltaTime;
             yield return null;
         }
+        moving = true;
     }
 }
+
